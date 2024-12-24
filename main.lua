deda535c9ed832c3f69f6dd9d11d213d5d4131ef
htk <htk.nguyen@outlook.fr>
2024-12-24 11:37:10 +0100

    import ds lib

---
+++ main.lua
@@ -0,0 +1,14 @@
+function love.load()
+   love.window.setMode(650, 650)
+   love.window.setTitle("elyrizz")
+end
+
+function love.update(dt)
+end
+
+function love.keypressed(key, scancode, isrepeat)
+
+end
+
+function love.draw()
+end
